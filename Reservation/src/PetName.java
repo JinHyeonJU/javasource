b66ca0308fd6a851dd4ed2143b12e62a081fbb5f
HyeonJu <guswn8557@gmail.com>
2020-11-03 15:42:51 +0900

    예약 프로그램

---
+++ Reservation/src/PetName.java
@@ -0,0 +1,11 @@
+
+public class PetName {
+	String petName;
+	
+	PetName(String petName) {
+		this.petName = petName;
+	}
+	public void diplayPetName() {
+		System.out.println("예약된 고양이 이름 : "+petName);
+	}
+}
---
+++ Reservation/src/Reservation.java
@@ -0,0 +1,8 @@
+
+public interface Reservation {
+	public static final String petName = "";
+	public static final int age = 0;
+	public static final int phoneNum = 0;
+	public static final String ReservationDay = "";
+	public static final String sex = "";
+}
