6b2120acedba17c7c3cea3699152cf854aa2267d
HyeonJu <guswn8557@gmail.com>
2020-11-03 15:01:36 +0900

    소스수정

--- 10.Exception/src/exception/ExceptionTest2.java
+++ 10.Exception/src/exception/ExceptionTest2.java
@@ -3,7 +3,13 @@ package exception;
 public class ExceptionTest2 {
 
 	public static void main(String[] args) {
-
+		String data1 = "100";
+		String data2 = "a100";
+		
+		int value1 = Integer.parseInt(data1);
+		int value2 = Integer.parseInt(data2); //a를 숫자로변경x
+		
+		
+		System.out.println(value1+value2);
 	}
-
 }
